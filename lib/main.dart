f14e15fda81d3a37cf83a3b197f39f4377c1a5a5
mertagca <mrtagcaa@gmail.com>
2023-07-15 15:58:14 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ModaApp());
+}
+
+class ModaApp extends StatelessWidget {
+  const ModaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Anasayfa(),
+    );
+  }
+}
+
+class Anasayfa extends StatefulWidget {
+  const Anasayfa({super.key});
+
+  @override
+  State<Anasayfa> createState() => _AnasayfaState();
+}
+
+class _AnasayfaState extends State<Anasayfa> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text(
+          "Moda Uygulaması",
+          style: TextStyle(
+              fontFamily: "Montserrat",
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+              color: Colors.black),
+        ),
+        actions: [
+          IconButton(
+              onPressed: () => {},
+              icon: Icon(
+                Icons.camera_alt,
+                color: Colors.grey,
+              )),
+        ],
+      ),
+      body: ListView(
+        padding: EdgeInsets.only(top: 10),
+        children: [
+          Container(
+            height: 140,
+            width: double.infinity,
+            child: ListView(
+              padding: EdgeInsets.all(10),
+              scrollDirection: Axis.horizontal,
+              children: [
+                listeElemani('assets/model1.jpeg', 'assets/chanellogo.jpg'),
+                SizedBox(
+                  width: 30,
+                ),
+                listeElemani('assets/model2.jpeg', 'assets/louisvuitton.jpg'),
+                SizedBox(
+                  width: 30,
+                ),
+                listeElemani('assets/model3.jpeg', 'assets/chloelogo.png'),
+                SizedBox(
+                  width: 30,
+                ),
+                listeElemani('assets/model1.jpeg', 'assets/chanellogo.jpg'),
+                SizedBox(
+                  width: 30,
+                ),
+                listeElemani('assets/model2.jpeg', 'assets/louisvuitton.jpg'),
+                SizedBox(
+                  width: 30,
+                ),
+                listeElemani('assets/model3.jpeg', 'assets/chloelogo.png'),
+                SizedBox(
+                  width: 30,
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget listeElemani(String imagePath, String logoPath) {
+    return Column(
+      children: [
+        Stack(
+          children: [
+            Container(
+              width: 75,
+              height: 75,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(38),
+                  image: DecorationImage(
+                    image: AssetImage(imagePath),
+                    fit: BoxFit.cover,
+                  )),
+            ),
+            Positioned(
+              top: 50,
+              left: 50,
+              child: Container(
+                width: 25,
+                height: 25,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    image: DecorationImage(
+                        image: AssetImage(logoPath), fit: BoxFit.contain)),
+              ),
+            )
+          ],
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          height: 30,
+          width: 75,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15), color: Colors.brown),
+          child: Center(
+            child: Text(
+              "Follow",
+              style: TextStyle(
+                  fontFamily: "Montserrat", fontSize: 14, color: Colors.white),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
